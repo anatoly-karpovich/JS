9bc1a8713a87cf66a30152b6d9328cf7a91fdd65
unknown <anatoly.karpovich90@gmail.com>
2021-10-17 21:27:08 +0300

    Changed array method to .reduce

--- Homework-1/task3-data.js
+++ Homework-1/task3-data.js
@@ -2,8 +2,11 @@
 // Итого: 8495 гривен, средняя цена товара 700 гривен - пример сообщения в консоле
 
 const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123];
-let sumPrices = 0;
-prices.forEach((price) => {
-sumPrices += +price;
-});
-console.log(`Итого: ${sumPrices} гривен, средняя цена товара ${sumPrices/prices.length} гривен`);
+
+function sumPrices(arr) {
+    let sum = arr.reduce ((a,b) => {
+        return a+b;
+    });
+    return sum;
+}
+console.log(`Итого: ${sumPrices(prices)} гривен, средняя цена товара ${sumPrices(prices)/prices.length} гривен`);
