a621b3c4284b92f4ff22ea35e48e4bb765a2ff00
unknown <anatoly.karpovich90@gmail.com>
2021-10-18 00:28:22 +0300

    Added task info

--- Codewars/kata1.js
+++ Codewars/kata1.js
@@ -1,4 +1,10 @@
+// Welcome.
 
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
 
 function alphabetPosition(text) {
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
