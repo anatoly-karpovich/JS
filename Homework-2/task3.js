2ceac5b7a1004b9826d042b0de428bb713263526
unknown <anatoly.karpovich90@gmail.com>
2021-10-18 14:16:28 +0300

    Added solution

---
+++ Homework-2/task3.js
@@ -0,0 +1,5 @@
+// There is an array with the most popular last names, but some first elements don’t look like valid last names. Delete the first two elements of the array.
+
+const arr =  ["НПП Прізвище Кіл-ть Приблизно","______________________в_ базіпо_Україні", "0-rt-rgfr", "8484fjdfkf", "МЕЛЬНИК","ШЕВЧЕНКО","БОЙКО","КОВАЛЕНКО","БОНДАРЕНКО","ТКАЧЕНКО","КОВАЛЬЧУК","КРАВЧЕНКО","ОЛІЙНИК"]
+arr.splice(0,4);
+console.log(arr);
