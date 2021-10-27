34dc099b3fde5c4c055725a1313e85659ec60cf7
unknown <anatoly.karpovich90@gmail.com>
2021-10-27 23:02:50 +0300

    changed code

--- Homework-3/task2/task2.js
+++ Homework-3/task2/task2.js
@@ -3,13 +3,10 @@ let persons;
 
 persons = JSON.parse(fs.readFileSync("./data.json"));
 let personsNew = [];
+
 persons.forEach((el) => {
-  if (
-    persons.filter((item) => {
-      return JSON.stringify(item) === JSON.stringify(el);
-    }).length < 2
-  ) {
-    personsNew.push(el);
+  if (personsNew.indexOf(JSON.stringify(el)) === -1) {
+    personsNew.push(JSON.stringify(el));
   }
 });
 console.log(personsNew.length);
